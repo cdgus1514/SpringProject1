9c1a95acd95ea7c4e341c707560e675b173e7d95
choidh <cdgus1514@gmail.com>
2021-10-30 20:41:49 +0900

    상품도메인 프로젝트1
    - 상품객체 및 상품 저장소(Repository) 생성
    - 테스트 코드 작성

---
+++ src/main/java/hello/springmvc/domain/item/ItemRepository.java
@@ -0,0 +1,46 @@
+package hello.springmvc.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+
+    // 싱글톤으로 생성되어서 HashMap 사용가능
+    // 동시에 여러쓰레드 접근 시 > ConcurrentHashMap 사용
+    private static final Map<Long, Item> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    // 저장
+    public Item save(Item item) {
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+
+        return item;
+    }
+
+    // 조회
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Item> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    // 수정
+    public void update(Long itemId, Item updateParam) {
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
